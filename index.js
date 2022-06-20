42d434f9056f388bcbd8aee9ad9d2341163fe4e4
David <davicum@gmail.com>
2022-06-20 07:50:57 +0200

    web scraping + json info

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+
+const url = 'https://www.coches.net/land-rover-range-rover-sport-36-tdv8-272-cv-hse-5p-diesel-2009-en-cordoba-52097826-covo.aspx'
+
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        console.log(html)
+
+    })
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
