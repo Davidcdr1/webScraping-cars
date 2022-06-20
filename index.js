74b7d7e7e116f1f178d788aa58d93fc3a4d6f607
David <davicum@gmail.com>
2022-06-20 21:42:03 +0200

    web scraping

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 
 
-const PORT = 8000
+const PORT = 8080
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 const app = express()
 
-const url = 'https://www.coches.net/land-rover-range-rover-sport-36-tdv8-272-cv-hse-5p-diesel-2009-en-cordoba-52097826-covo.aspx'
+ const url = 'https://www.coches.net/land-rover-range-rover-sport-36-tdv8-272-cv-hse-5p-diesel-2009-en-cordoba-52097826-covo.aspx';
+
+
 
 
 axios(url)
     .then(response => {
         const html = response.data;
-        console.log(html);
+        const $ = cheerio.load(html)
 
-    })
+        const articles = []
+
+        $('.mt-PanelAdInfo', html).each((index, element) =>  { 
+            const brand = $(element).children('.mt-PanelAdInfo-title').text()
+            const price = $(element).children('.mt-PanelAdInfo-price').text()
+
+            articles.push({
+                brand,
+                price
+            })
+        })
+        
+        console.log(articles)
+    
+        
+      }).catch(err => console.log(err))
+    
+
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
