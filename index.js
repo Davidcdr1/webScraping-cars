5069f773ced1d6cc4e347727e67b46cd07709bd5
David <davicum@gmail.com>
2022-06-21 12:48:46 +0200

    web scraping

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const PORT = 8080
+const PORT = 3000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
@@ -10,31 +10,34 @@ const app = express()
 
 
 
+ 
 
-axios(url)
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html)
 
-        const articles = []
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
 
-        $('.mt-PanelAdInfo', html).each((index, element) =>  { 
-            const brand = $(element).children('.mt-PanelAdInfo-title').text()
-            const price = $(element).children('.mt-PanelAdInfo-price').text()
-
-            articles.push({
-                brand,
-                price
+            $('.mt-PanelAdInfo', html).each(function () { 
+                const Brand = $('.mt-PanelAdInfo-title').text();
+                const Details = $('.mt-PanelAdDetails-dataItem').text();
+                const Price = $('.mt-PanelAdInfo-price').text();
+                const Description = $('.mt-PanelAdDetails-commentsContent').text();
+                const UrlImage = $('.mt-GalleryBasic-sliderImage').attr('src');
+                articles.push({
+                    Brand,
+                    Details,
+                    Price,
+                    Description,
+                    UrlImage
+                })
             })
-        })
-        
-        console.log(articles)
-    
-        
-      }).catch(err => console.log(err))
-    
+            console.log(articles)
+
+        }).catch(err => console.log(err))
 
 
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+       
 
